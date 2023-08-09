b26b12bbc31ee28e7820cd4752726e7a764b173f
MarcLel <marclelarge92@gmail.com>
2023-08-09 10:31:15 +0200

    resolution  du probleme

--- public/index.php
+++ public/index.php
@@ -16,13 +16,14 @@ define('VIEW_PATH', dirname(__DIR__) . '/views');
 //var_dump(VIEW_PATH);
 
 $router->map('GET', '/', function() {
-    require VIEW_PATH . '/post/index.php';
-}, 'home');
-
-//var_dump(VIEW_PATH . '/category/show.php');
-$router->map('GET', '/category', function() {
     require VIEW_PATH . '/category/show.php';
 }, 'category');
+
+//var_dump(VIEW_PATH . '/category/show.php');
+$router->map('GET', '/index', function() {
+    require VIEW_PATH . '/post/index.php';
+}, 'Home');
+
 
 $match = $router->match();
 

696b835d5c6653f42e5826c4281e665a5be0b19e
MarcLel <marclelarge92@gmail.com>
2023-08-10 10:06:59 +0200

    ajout commentaire

--- public/index.php
+++ public/index.php
@@ -1,35 +1,39 @@
 <?php 
 
-// autoload
-
+// Inclusion du fichier d'autoload pour charger automatiquement les classes nécessaires
 require '../vendor/autoload.php';
 
+// Création d'un gestionnaire d'erreurs Whoops pour un meilleur rapport d'erreurs
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-// start altorouter 
+// Initialisation de AltoRouter pour gérer les routes
 $router = new AltoRouter();
 
+// Définition d'une constante pour le chemin du répertoire des vues
 define('VIEW_PATH', dirname(__DIR__) . '/views');
 
-//var_dump(VIEW_PATH);
-
+// Association d'une route pour l'URL racine ("/") à une fonction de rappel
 $router->map('GET', '/', function() {
+    // Inclusion du fichier de vue 'show.php' pour la route 'category'
     require VIEW_PATH . '/category/show.php';
 }, 'category');
 
-//var_dump(VIEW_PATH . '/category/show.php');
+// Association d'une route pour l'URL '/index' à une fonction de rappel
 $router->map('GET', '/index', function() {
+    // Inclusion du fichier de vue 'index.php' pour la route 'Home'
     require VIEW_PATH . '/post/index.php';
 }, 'Home');
 
-
+// Tentative de correspondance de l'URL actuelle à une route définie dans le routeur
 $match = $router->match();
 
+// Si une route correspondante est trouvée
 if ($match) {
+    // Appel de la fonction de rappel associée à la route correspondante
     call_user_func($match['target']);
 } else {
+    // Affichage d'un message d'erreur 404 si aucune route correspondante n'est trouvée
     echo "Page non trouvée (404)";
 }
-
--- src/Router.php
+++ src/Router.php
@@ -11,25 +11,25 @@ class Router {
          * @var AltoRouter
          */
 
-    public function __construct(string $viewPath)
-    {
-        $this->viewPath = $viewPath;
-        $this->router = new \AltoRouter();
-    }
+         public function __construct(string $viewPath)
+         {
+             $this->viewPath = $viewPath; // Attribue le chemin du répertoire des vues passé en paramètre à la propriété viewPath de l'objet
+             $this->router = new \AltoRouter(); // Crée une nouvelle instance de la classe AltoRouter et l'assigne à la propriété router de l'objet
+         }
 
-    public function get(string $url, string $view, ?string $name = null): self
+    public function run(): self
     {
-        $this->router->map('GET', $url, $view, $name);
-        return $this;
-    }
+        $match = $this->router->match(); // Tente de faire correspondre l'URL actuelle à une route définie
 
-    public function run() : self
-    {
-        $match = $this->router->match();
-        $views = $match['target'];
-        $router = $this;
-        require $this->viewPath . $view . '.php';
+        if ($match) {
+            $view = $match['target']; // Récupère la vue associée à la route correspondante
+            $router = $this; // Stocke une référence à l'objet Router dans la variable locale $router
+            require $this->viewPath . $view . '.php'; // Inclut la vue associée à la route correspondante
 
-        return $this;
+        } else {
+            echo "Page non trouvée (404)"; // Affiche un message d'erreur si aucune route correspondante n'est trouvée
+        }
+
+        return $this; // Retourne l'objet Router
     }
 }
